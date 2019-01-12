84e05cccb7eaef4aaa8b994297a011dd9153af9c
kvmanohar22 <kvmanohar22@gmail.com>
2019-01-12 20:13:34 +0530

    update model

--- nnet/model.py
+++ nnet/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 import chainer
 import chainer.functions as F
@@ -33,79 +34,73 @@ class EncoderDecoder(chainer.Chain):
          self.conv5_2 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=3, stride=1, pad=1)
          self.conv5_3 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=3, stride=1, pad=1)
 
-         # self.conv6_1 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=1, stride=1, pad=1)
+         self.conv6_1 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=1, stride=1, pad=1)
          self.bn5     = L.BatchNormalization(self.out_c*8)
          self.dconv5  = L.Deconvolution2D(None, self.out_c*8, ksize=9)
 
-         self.conv7_1 = L.Convolution2D(self.out_c*16, self.out_c*8, ksize=5, stride=1, pad=1)
+         self.conv7_1 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=5, stride=1, pad=1)
          self.bn4     = L.BatchNormalization(self.out_c*8)
          self.dconv4  = L.Deconvolution2D(None, self.out_c*8, ksize=3)
 
-         self.conv8_1 = L.Convolution2D(self.out_c*16, self.out_c*4, ksize=5, stride=1, pad=1)
+         self.conv8_1 = L.Convolution2D(self.out_c*8, self.out_c*4, ksize=5, stride=1, pad=1)
          self.bn3     = L.BatchNormalization(self.out_c*4)
          self.dconv3  = L.Deconvolution2D(None, self.out_c*4, ksize=5, pad=1)
 
-         self.conv9_1 = L.Convolution2D(self.out_c*8, self.out_c*2, ksize=5, stride=1, pad=1)
+         self.conv9_1 = L.Convolution2D(self.out_c*4, self.out_c*2, ksize=5, stride=1, pad=1)
          self.bn2     = L.BatchNormalization(self.out_c*2)
          self.dconv2  = L.Deconvolution2D(None, self.out_c*2, ksize=3)
 
-         self.convf_1 = L.Convolution2D(self.out_c*4, self.out_c*1, ksize=5, stride=1, pad=1)
+         self.convf_1 = L.Convolution2D(self.out_c*2, self.out_c*1, ksize=5, stride=1, pad=1)
          self.bn1     = L.BatchNormalization(self.out_c*1)
          self.dconv1  = L.Deconvolution2D(None, self.out_c*1, ksize=3)
 
-         self.conv0_1 = L.Convolution2D(self.out_c*2, self.out_c, ksize=5, stride=1, pad=1)
+         self.conv0_1 = L.Convolution2D(self.out_c*1, self.out_c, ksize=5, stride=1, pad=1)
          self.bn0     = L.BatchNormalization(self.out_c)
 
-         self.convv   = L.Convolution2D(self.out_c, 1, ksize=5, stride=1, pad=1)
+         self.convv   = L.Convolution2D(self.out_c, 1, ksize=1, stride=1, pad=1)
 
    def __call__(self, x):
       x = F.relu(self.conv1_1(x))
-      c1 = F.relu(self.conv1_2(x))
-      x = F.max_pooling_2d(c1, ksize=2, stride=2)
+      x = F.relu(self.conv1_2(x))
+      x = F.max_pooling_2d(x, ksize=2, stride=2)
 
       x = F.relu(self.conv2_1(x))
-      c2 = F.relu(self.conv2_2(x))
-      x = F.max_pooling_2d(c2, ksize=2, stride=2)
+      x = F.relu(self.conv2_2(x))
+      x = F.max_pooling_2d(x, ksize=2, stride=2)
 
       x = F.relu(self.conv3_1(x))
       x = F.relu(self.conv3_2(x))
-      c3 = F.relu(self.conv3_3(x))
-      x = F.max_pooling_2d(c3, ksize=2, stride=2)
+      x = F.relu(self.conv3_3(x))
+      x = F.max_pooling_2d(x, ksize=2, stride=2)
 
       x = F.relu(self.conv4_1(x))
       x = F.relu(self.conv4_2(x))
-      c4 = F.relu(self.conv4_3(x))
-      x = F.max_pooling_2d(c4, ksize=2, stride=2)
+      x = F.relu(self.conv4_3(x))
+      x = F.max_pooling_2d(x, ksize=2, stride=2)
 
       x = F.relu(self.conv5_1(x))
       x = F.relu(self.conv5_2(x))
-      c5 = F.relu(self.conv5_3(x))
-      x = F.max_pooling_2d(c5, ksize=2, stride=2)
+      x = F.relu(self.conv5_3(x))
+      x = F.max_pooling_2d(x, ksize=2, stride=2)
 
       x = F.relu(self.bn5(self.conv6_1(x)))
       x = self.dconv5(x)
-      x = F.concat((c5, x))
       x = F.unpooling_2d(x, stride=2, ksize=5, pad=1)
 
       x = F.relu(self.bn4(self.conv7_1(x)))
       x = self.dconv4(x)
-      x = F.concat((c4, x))
       x = F.unpooling_2d(x, stride=2, ksize=5, pad=1)
 
       x = F.relu(self.bn3(self.conv8_1(x)))
       x = self.dconv3(x)
-      x = F.concat((c3, x))
       x = F.unpooling_2d(x, stride=2, ksize=5, pad=1)
 
       x = F.relu(self.bn2(self.conv9_1(x)))
       x = self.dconv2(x)
-      x = F.concat((c2, x))
       x = F.unpooling_2d(x, stride=2, ksize=5, pad=1)
 
       x = F.relu(self.bn1(self.convf_1(x)))
       x = self.dconv1(x)
-      x = F.concat((c1, x))
-      x = F.unpooling_2d(x, stride=2, ksize=5, pad=1)
 
       x = F.relu(self.bn0(self.conv0_1(x)))
       x = F.sigmoid(self.convv(x))
@@ -115,4 +110,5 @@ class EncoderDecoder(chainer.Chain):
 if __name__ == '__main__':
    model = EncoderDecoder().to_gpu()
    x = np.zeros((1, 4, 320, 320), dtype=np.float32)
+   s = time.time()
    print(model(to_gpu(x)).shape)
