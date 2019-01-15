3005d7072d8513285f157ec14d9966c1ced24bdf
kvmanohar22 <kvmanohar22@gmail.com>
2019-01-15 21:04:34 +0530

    load vgg weights

--- nnet/model.py
+++ nnet/model.py
@@ -5,9 +5,14 @@ import time
 import chainer
 import chainer.functions as F
 import chainer.links as L
-
 from chainer.backends.cuda import to_gpu
 from chainer.backends.cuda import to_cpu
+from chainer.initializers import GlorotUniform
+from chainer.initializers import LeCunUniform
+
+from chainercv.links import VGG16
+
+from DIM.options import Options
 
 class EncoderDecoder(chainer.Chain):
    def __init__(self, opts):
@@ -37,30 +42,39 @@ class EncoderDecoder(chainer.Chain):
          self.conv5_2 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=3, stride=1, pad=1)
          self.conv5_3 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=3, stride=1, pad=1)
 
-         self.conv6_1 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=1, stride=1, pad=1)
+         self.conv6_1 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=1, stride=1, pad=1,
+                                        initialW=None)
          self.bn5     = L.BatchNormalization(self.out_c*8)
-         self.dconv5  = L.Deconvolution2D(None, self.out_c*8, ksize=9)
+         self.dconv5  = L.Deconvolution2D(None, self.out_c*8, ksize=9, initialW=None)
 
-         self.conv7_1 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=5, stride=1, pad=1)
+         self.conv7_1 = L.Convolution2D(self.out_c*8, self.out_c*8, ksize=5, stride=1, pad=1,
+                                        initialW=None)
          self.bn4     = L.BatchNormalization(self.out_c*8)
-         self.dconv4  = L.Deconvolution2D(None, self.out_c*8, ksize=3)
+         self.dconv4  = L.Deconvolution2D(None, self.out_c*8, ksize=3, initialW=None)
 
-         self.conv8_1 = L.Convolution2D(self.out_c*8, self.out_c*4, ksize=5, stride=1, pad=1)
+         self.conv8_1 = L.Convolution2D(self.out_c*8, self.out_c*4, ksize=5, stride=1, pad=1,
+                                        initialW=None)
          self.bn3     = L.BatchNormalization(self.out_c*4)
-         self.dconv3  = L.Deconvolution2D(None, self.out_c*4, ksize=5, pad=1)
+         self.dconv3  = L.Deconvolution2D(None, self.out_c*4, ksize=5, initialW=None, pad=1)
 
-         self.conv9_1 = L.Convolution2D(self.out_c*4, self.out_c*2, ksize=5, stride=1, pad=1)
+         self.conv9_1 = L.Convolution2D(self.out_c*4, self.out_c*2, ksize=5, stride=1, pad=1,
+                                        initialW=None)
          self.bn2     = L.BatchNormalization(self.out_c*2)
-         self.dconv2  = L.Deconvolution2D(None, self.out_c*2, ksize=3)
+         self.dconv2  = L.Deconvolution2D(None, self.out_c*2, ksize=3, initialW=None)
 
-         self.convf_1 = L.Convolution2D(self.out_c*2, self.out_c*1, ksize=5, stride=1, pad=1)
+         self.convf_1 = L.Convolution2D(self.out_c*2, self.out_c*1, ksize=5, stride=1, pad=1,
+                                        initialW=None)
          self.bn1     = L.BatchNormalization(self.out_c*1)
-         self.dconv1  = L.Deconvolution2D(None, self.out_c*1, ksize=3)
+         self.dconv1  = L.Deconvolution2D(None, self.out_c*1, ksize=3, initialW=None)
 
-         self.conv0_1 = L.Convolution2D(self.out_c*1, self.out_c, ksize=5, stride=1, pad=1)
+         self.conv0_1 = L.Convolution2D(self.out_c*1, self.out_c, ksize=5, stride=1, pad=1,
+                                        initialW=None)
          self.bn0     = L.BatchNormalization(self.out_c)
 
-         self.convv   = L.Convolution2D(self.out_c, 1, ksize=1, stride=1, pad=1)
+         self.convv   = L.Convolution2D(self.out_c, 1, ksize=1, stride=1, pad=1, initialW=None)
+
+      # Load VGG network weights
+      self.load_vgg_weights()
 
    def __call__(self, x):
       x = F.relu(self.conv1_1(x))
@@ -110,6 +124,30 @@ class EncoderDecoder(chainer.Chain):
 
       return x
 
+   def load_vgg_weights(self):
+      """ Loads the vgg weights
+      """
+      print('Loading VGG weights onto encoder...', end=' ')
+      vgg = VGG16(pretrained_model='imagenet')
+      # self.conv1_1.copyparams(vgg.conv1_1.conv)
+      self.conv1_2.copyparams(vgg.conv1_2.conv)
+
+      self.conv2_1.copyparams(vgg.conv2_1.conv)
+      self.conv2_2.copyparams(vgg.conv2_2.conv)
+
+      self.conv3_1.copyparams(vgg.conv3_1.conv)
+      self.conv3_2.copyparams(vgg.conv3_2.conv)
+      self.conv3_3.copyparams(vgg.conv3_3.conv)
+
+      self.conv4_1.copyparams(vgg.conv4_1.conv)
+      self.conv4_2.copyparams(vgg.conv4_2.conv)
+      self.conv4_3.copyparams(vgg.conv4_3.conv)
+
+      self.conv5_1.copyparams(vgg.conv5_1.conv)
+      self.conv5_2.copyparams(vgg.conv5_2.conv)
+      self.conv5_3.copyparams(vgg.conv5_3.conv)
+      print('Done')
+
    def loss(self, outputs, targets, trimaps):
       """ Compute the two losses
       """
@@ -134,7 +172,7 @@ class EncoderDecoder(chainer.Chain):
             targets: Target RGB
          """
          eps = cp.zeros_like(outputs, dtype=cp.float32) + self.epsilon
-         loss = F.sqrt(F.square(outputs - targets) + F.square(eps)) / 255.0
+         loss = F.sqrt(F.square(outputs - targets) + F.square(eps))
          loss = F.mean(F.sum(loss * wl, axis=(1, 2, 3)) / F.sum(wl, axis=(1, 2, 3)))
          return loss
 
@@ -151,7 +189,10 @@ class EncoderDecoder(chainer.Chain):
       return self.alpha * l1 + (1 - self.alpha) * l2
 
 if __name__ == '__main__':
-   model = EncoderDecoder().to_gpu()
-   x = np.zeros((1, 4, 320, 320), dtype=np.float32)
-   s = time.time()
-   print(model(to_gpu(x)).shape)
+   opts = Options().parse()
+   model = EncoderDecoder(opts)
+   x = np.zeros((1, 4, opts["H"], opts["W"]), dtype=np.float32)
+   if opts["gpu_id"] >= 0:
+      model.to_gpu()
+      x = to_gpu(x)
+   print(model(x).shape)
