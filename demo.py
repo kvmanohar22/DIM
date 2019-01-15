f1821115a53803f75a02f3bcb5f54a1f0c4b4c6d
kvmanohar22 <kvmanohar22@gmail.com>
2019-01-15 22:26:47 +0530

    complete demo

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ from skimage import io
 from nnet.model import EncoderDecoder
 from options import Options
 from utils.loader import Loader, Preprocess
-from utils.utils import extract_FG_BG, blend, CHW2HWC
+from utils.utils import extract_FG_BG, blend, CHW2HWC, MEAN
 from utils.utils import load_image, load_trimap
 from utils.utils import mkdirs
 
@@ -20,7 +20,7 @@ from chainer.backends import cuda
 from chainer.backends.cuda import to_gpu
 from chainer.backends.cuda import to_cpu
 
-def save(args, epoch, idx, root):
+def save(pred_matte, pred_RGB, blends):
    p_alp = CHW2HWC(args[0])
    t_alp = CHW2HWC(args[1])
    p_RGB = CHW2HWC(args[2])
@@ -29,14 +29,7 @@ def save(args, epoch, idx, root):
    mkdirs([osp.join(root, 'epoch_{}'.format(epoch))])
 
    io.imsave(osp.join(root, "epoch_{}".format(epoch), "t_alp_iidx_{}_idx_{}.png".format(idx, 0)), np.squeeze(t_alp[0]))
-   io.imsave(osp.join(root, "epoch_{}".format(epoch), "p_alp_iidx_{}_idx_{}.png".format(idx, 0)), np.squeeze(p_alp[0]))
    io.imsave(osp.join(root, "epoch_{}".format(epoch), "t_RGB_iidx_{}_idx_{}.png".format(idx, 0)), t_RGB[0])
-   io.imsave(osp.join(root, "epoch_{}".format(epoch), "p_RGB_iidx_{}_idx_{}.png".format(idx, 0)), p_RGB[0])
-
-   io.imsave(osp.join(root, "epoch_{}".format(epoch), "t_alp_iidx_{}_idx_{}.png".format(idx, 1)), np.squeeze(t_alp[1]))
-   io.imsave(osp.join(root, "epoch_{}".format(epoch), "p_alp_iidx_{}_idx_{}.png".format(idx, 1)), np.squeeze(p_alp[1]))
-   io.imsave(osp.join(root, "epoch_{}".format(epoch), "t_RGB_iidx_{}_idx_{}.png".format(idx, 1)), t_RGB[1])
-   io.imsave(osp.join(root, "epoch_{}".format(epoch), "p_RGB_iidx_{}_idx_{}.png".format(idx, 1)), p_RGB[1])
 
 def main(opts):
    H = opts["H"]
@@ -60,14 +53,12 @@ def main(opts):
    images, trimaps = Preprocess(H, W).resize((img, tri, None), (H, W))
 
    inputs = np.empty((1, 4, H, W), dtype=np.float32)
-   inputs[0, :3, ...] = images
+   inputs[0, :3, ...] = images - MEAN
    inputs[0, 3:, ...] = trimaps
 
    # Transfer to GPU
    if opts["gpu_id"] >= 0:
       inputs  = to_gpu(inputs) / 255.0
-      images  = to_gpu(images) / 255.0
-      trimaps = to_gpu(trimaps)
 
    # Forward pass
    with chainer.using_config('train', False), \
@@ -79,22 +70,40 @@ def main(opts):
    # Predict RGB
    predicted_matte = (to_cpu(predictions.data) * 255).astype(np.uint8)
    predicted_alp = to_cpu(predictions.data)
-   predicted_matte = np.where(np.equal(to_cpu(trimaps), 128),
+   predicted_matte = np.where(np.equal(trimaps, 128),
                               predicted_alp,
-                              to_cpu(trimaps))
+                              trimaps / 255)
 
-   fgs, bgs = extract_FG_BG(images, outputs)
-   predicted_RGB = (blend((predicted_matte*255).astype(np.uint8), fgs, bgs) / 255.0).astype(np.float32)
+   fgs, bgs = extract_FG_BG(images, predicted_alp)
+   predicted_matte = (predicted_matte*255).astype(np.uint8)
+   predicted_RGB = (blend(predicted_matte,
+         fgs, bgs)).astype(np.uint8)
 
-   if opts["gpu_id"] >= 0:
-      predicted_RGB = to_gpu(predicted_RGB)
+   # Save
+   print('Saving predicted matte and RGB...', end=' ')
+   io.imsave(opts["img_path"].replace(".png", "_predicted_matte.png"), np.squeeze(predicted_matte[0]))
+   io.imsave(opts["img_path"].replace(".png", "_predicted_RGB.png"), CHW2HWC(predicted_RGB)[0])
+   print('Done')
 
-   #    # Save outputs
-   #    args = [(predicted_matte * 255).astype(np.uint8), 
-   #            (to_cpu(outputs) * 255).astype(np.uint8),
-   #            (to_cpu(predicted_RGB) * 255).astype(np.uint8),
-   #            (to_cpu(images) * 255).astype(np.uint8)]
-   #    save(args, epoch, idx, opts["log_root"])
+   new_bgs = os.listdir(opts["newimgs_dir"])
+   if len(new_bgs) > 0:
+      for idx, bg in enumerate(new_bgs):
+         print("Processing {:2d}/{:2d} images".format(idx+1, len(new_bgs)))
+         img_path = osp.join(opts["newimgs_dir"], bg)
+         extension = img_path.split('.')[-1]
+
+         # Load new image
+         img = load_image(img_path)
+         org_h, org_w = img.shape[1:]
+         images, trimaps = Preprocess(H, W).resize((img, tri, None), (H, W))
+
+         # Blend
+         predicted_RGB = (blend(predicted_matte,
+               fgs, images)).astype(np.uint8)
+         images, trimaps = Preprocess(H, W).resize((predicted_RGB[0], tri, None), (org_h, org_w))
+
+         # Save the image
+         io.imsave(img_path.replace(".{}".format(extension), "_predicted_RGB.png"), images[0])
 
 if __name__ == '__main__':
    opts = Options().parse(train_mode=False)
